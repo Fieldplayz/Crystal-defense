971817bab94918bce1fb7bfc6cfe0a27b69d305e
GhostN1ck <vanviegennick@gmail.com>
2022-12-01 12:26:31 +0100

    CrystalScript v1

--- Crystal defense/Assets/Scripts/CrystalHealth.cs
+++ Crystal defense/Assets/Scripts/CrystalHealth.cs
@@ -5,21 +5,45 @@ using UnityEngine.UI;
 
 public class CrystalHealth : MonoBehaviour
 {
+    [Header("Health Bar :")]
     public Slider healthBar;
 
+    [Header("Max Health Amount :")]
     [Range(0.1f, 10f)]
-    public float maxHealth = 10f;
+    public float maxHealth;
+
+    [Header("Enemy Damage Amount :")]
+    [Range(0.1f, 5f)]
+    public float enemyDamageValue;
+
+    [Header("Damage Cooldown Time :")]
+    [Range(0.1f, 5f)]
+    public float damageCooldownTime;
 
     public float currentHealth;
+    public bool isDamageable;
 
     private void Start()
     {
+        isDamageable = true;
         currentHealth = maxHealth;
         healthBar.value = currentHealth;
     }
 
-    public void TakeDamage(float amount)
+    IEnumerator TakeDamage(float damage)
     {
+        isDamageable = false;
+        yield return new WaitForSeconds(damageCooldownTime);
+        currentHealth -= damage;
+        healthBar.value = currentHealth;
+        isDamageable = true;
+    }
 
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage(enemyDamageValue);
+        }
     }
 }
